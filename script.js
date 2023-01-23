c7d32317a359e584da53339de0ca6437c0bf7941
magic404 <jamesbox3@gmail.com>
2023-01-23 14:49:04 -0800

    updates to script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let myAPI = '12b731178b784355e0fe1830a2375d2b';
+let cityHistory = [];
 
+let weatherForm = document.getElementById("weather-form");
+let weatherInput = document.getElementById("weather-input");
+let currentContainer = document.getElementById("current");
+let fiveDayContainer = document.getElementById("five-day");
+let historyContainer = document.getElementById("history");
