6cdb64b273a48c786e81e0a5736653d82e6ddc1e
magic404 <jamesbox3@gmail.com>
2023-01-30 12:35:05 -0800

    Changes to script .JS

--- script.js
+++ script.js
@@ -7,8 +7,53 @@ let currentContainer = document.getElementById("current");
 let fiveDayContainer = document.getElementById("five-day");
 let historyContainer = document.getElementById("history");
 
+function addToHistory () {
+    // to be completed
+}
+
+function displayWeather(city,  data) {
+  // to be completed
+}
+function getWeatherUsingApi (city,data) {
+    // to be completed
+    let {lat} = data;
+    let {lon} = data;
+    let wUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={myAPI}&units=imperial`
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${myAPI}`;
+    fetch (weatherUrl)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data){
+            console.log( "data = ", data);
+            displayWeather(city, data);
+        })
+        .catch ( function ( error) {
+            console.error ( error);
+        })
+
+}
+
 function getGeoCoordinatesUsingAPI(city) {
     // to  be completed
+    let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${myAPI}`;
+    fetch (geoUrl) 
+        .then ( function (resp) {
+            return resp.json();
+        })
+        .then ( function(jsonData){
+            if(!jsonData[0]) {
+                alert('Check the city name, not found');
+            } else {
+                console.log('jsonData  = ', jsonData);
+                addToHistory(city);
+                getWeatherUsingApi(city,jsonData[0]);
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
 }
 function searchHandler(e) {
     if(!weatherInput.value) {
